0e8ecd127bed5772c92bdf8e5e943df7d4d4c84d
Daxton <s02048898067@gmail.com>
2021-07-27 06:57:15 +0800

    1

---
+++ src/main/java/com/daxton/fancystrengthen/FancyStrengthen.java
@@ -0,0 +1,17 @@
+package com.daxton.fancystrengthen;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FancyStrengthen extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
